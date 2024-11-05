b317b004f4b6251f879255717351c8815e45af05
marcosabdalla <48099375+marcosabdalla@users.noreply.github.com>
2024-11-05 16:11:41 -0300

    Update GeraQRCODE.py

--- GeraQRCODE.py
+++ GeraQRCODE.py
@@ -1,13 +1,23 @@
 import qrcode
 import streamlit as st
+import io
 
 # Text input area
 txt = st.empty()
 code = txt.text_input(label="Texto para o QR-Code")
-
-bt = st.button('Gerar QR-Code')
-
-if bt:
+if code:
     meu_qrcode = qrcode.make(code)
-    meu_qrcode.save("CodigoQR.png")
+
+    # Save the QR code image to an in-memory buffer
+    img_buffer = io.BytesIO()
+    meu_qrcode.save(img_buffer, format="PNG")
+    img_buffer.seek(0)
+
+    # Create a download button for the QR code
+    btn = st.download_button(
+        label="Baixar QR-Code",
+        data=img_buffer,
+        file_name="qrcode.png",
+        mime="image/png"
+    )
     txt.text_input("",value="")
